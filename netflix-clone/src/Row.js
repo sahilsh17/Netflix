f5f61538269cd4930f60e78039d972a3fc45885d
Sahil Sharma <sahil.sharma237333@gmail.com>
2022-11-09 09:41:36 -0800

    created Row.css file

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,13 +1,14 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
 
 const baseURL = "https://image.tmdb.org/t/p/original/";
-function Row({title,fetchURL}) {
+function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition.
   // if [], run once when the row loads, and don't run again.
-  useEffect(()=> {
+  useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
       console.log(request);
@@ -16,18 +17,18 @@ function Row({title,fetchURL}) {
     }
     fetchData();
   }, [fetchURL]);
-  
+
   console.log(movies);
   return (
-    <div className="row">   
-     <h2> {title}</h2>
+    <div className="row">
+      <h2> {title}</h2>
 
-     <div className="row__posters">
-      {/* {several row posters} */}
-      {movies.map(movie => (
-        <img src={`${baseURL}${movie.poster_path}`} alt={movie.name} />
-      ))}
-     </div>
+      <div className="row__posters">
+        {/* {several row posters} */}
+        {movies.map(movie => (
+          <img src={`${baseURL}${movie.poster_path}`} alt={movie.name} className="row__poster" />
+        ))}
+      </div>
     </div>
   )
 }
