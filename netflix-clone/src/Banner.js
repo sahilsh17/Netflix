7b2e445538b0f9ff4ae81208e5bcc7439a789a97
Sahil Sharma <sahil.sharma237333@gmail.com>
2022-11-09 20:37:24 -0800

    customizing banner

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -8,12 +8,27 @@ function Banner() {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
-      console.log(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+      setMovie(request.data.results[Math.floor(Math.random() * request.data.results.length - 1)]);
+
+      return request;
     }
     fetchData();
   }, [])
   return (
-    <header>
+    <header className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+        "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
+      )`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+      </div>
       {/* {background image} */}
       {/* {title} */}
       {/* {div 2 btns} */}
