cd1fef1a0eacc23c860b92e5ffc1eef095c2b8fd
Sahil Sharma <sahil.sharma237333@gmail.com>
2022-11-29 17:12:50 -0800

    adding trailer

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const baseURL = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
   // A snippet of code which runs based on a specific condition.
   // if [], run once when the row loads, and don't run again.
@@ -18,7 +21,27 @@ function Row({ title, fetchURL, isLargeRow }) {
     fetchData();
   }, [fetchURL]);
 
-  console.log(movies);
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    }
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then(url => {
+          console.log(url);
+          console.log(movie.name);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(urlParams);
+          setTrailerUrl(urlParams.get('v'));
+        }).catch(error => console.log(error))
+    }
+  };
   return (
     <div className="row">
       <h2> {title}</h2>
@@ -28,9 +51,11 @@ function Row({ title, fetchURL, isLargeRow }) {
         {movies.map(movie => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} className={`row__poster ${isLargeRow && "row__posterLarge"}`} />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
