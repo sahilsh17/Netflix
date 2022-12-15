e3ef99762b557ccbbf9ce931f4c8e2d6db51aa71
Sahil Sharma <sahil.sharma237333@gmail.com>
2022-12-15 15:48:45 -0800

    added firebase, hosted the project on firebase and installed react-router-dom

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBDrbHWR0Yr8r0AituRH9DlzlL5dgwwi74",
+  authDomain: "netflix-clone-6a30b.firebaseapp.com",
+  projectId: "netflix-clone-6a30b",
+  storageBucket: "netflix-clone-6a30b.appspot.com",
+  messagingSenderId: "1042381325911",
+  appId: "1:1042381325911:web:273b02babf7004288b0512",
+  measurementId: "G-T3EPRPC6D7"
+};
