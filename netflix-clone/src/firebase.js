dbfef01dbb2403b9e3e439dd506652faca6bc5a6
Sahil Sharma <sahil.sharma237333@gmail.com>
2023-01-03 01:21:10 -0800

    signin and register functions added

--- netflix-clone/src/firebase.js
+++ netflix-clone/src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBDrbHWR0Yr8r0AituRH9DlzlL5dgwwi74",
